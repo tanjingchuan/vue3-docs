90c8871b83148ebd576315c0ae722f0ad6e47e6a
谭京川 <tanjingchuan@meicai.cn>
2020-11-13 16:32:48 +0800

    feat: 初始化文档项目结构

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  title: 'Vue3源码系列',
+  description: '针对vue3的新特性做一些源码分析和实例应用',
+  head: [
+    ['link', { rel: 'icon', href: '/logo.png' }]
+  ],
+  themeConfig: {
+    nav: [
+      { text: '新特性', link: '/feature/introduce'},
+      { text: '迁移最佳实践', link: '/migrate/'}
+    ],
+    sidebar: {
+      '/feature/': [
+        'introduce',
+        'composition-api'
+      ]
+    },
+    logo: '/logo.png',
+    sidebarDepth: 2,
+    displayAllHeaders: true
+  }
+}
---
+++ docs/README.md
@@ -0,0 +1,15 @@
+---
+home: true
+heroText: Vue3新特性
+tagline: 针对Vue3的新特性进行源码的解读以及日常应用开发的说明
+actionText: 现在开始 →
+actionLink: /feature/introduce/
+features:
+- title: 特性说明
+  details: 对vue3新特性的功能和用法进行说明
+- title: 源码解读
+  details: 从源码的角度带你了解vue3各种特性的实现
+- title: 实际应用
+  details: 对于新特性的说明，还添加了许多通俗易懂的DEMO实例
+footer: MIT Licensed | Copyright © 2020-present
+---
